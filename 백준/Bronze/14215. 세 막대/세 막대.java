25a039ed9944a58f823d1a079b8c746c71770905
heesancreat <95956131+heesancreat@users.noreply.github.com>
2024-07-15 16:05:08 +0900

    [Bronze III] Title: 세 막대, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,31 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int [] A = new int[3];
+		
+		for(int i =0; i<3;i++) {
+			A[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(A);
+		
+		if(A[0]+A[1]>A[2]) {
+			System.out.println(A[0]+A[1]+A[2]);
+		}else {
+			int plus=A[2]-(A[0]+A[1])+1;
+			System.out.println(A[0]+A[1]+A[2]-plus);
+		}
+		
+
+		
+	}
+
+}
