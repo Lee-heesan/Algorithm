ada33afa8e1f95b270b58c96bbb789fcf2bf9561
heesancreat <95956131+heesancreat@users.noreply.github.com>
2024-07-19 16:27:56 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.29 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,36 +1,20 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 class Solution {
-    public static Integer maxKey, minKey;
-	public static Integer maxValue=-1,minValue=51;
     public int[] solution(String[] wallpaper) {
-        Map<Integer, List<Integer>> arr = new HashMap<Integer, List<Integer>>(); 
-		
-		for(int i =0; i<wallpaper.length;i++) {
-			if(wallpaper[i].contains("#")) {
-				for(int j=0;j<wallpaper[i].length();j++){
-					if(wallpaper[i].charAt(j)=='#') {
-						arr.put(i, new ArrayList<Integer>());
-						arr.get(i).add(j);
-						if(j<minValue) {
-							minValue=j;
-						}
-						if(j+1>maxValue) {
-							maxValue=j+1;
-						}
-					}
-				}
-			}
-		}
-		
-		maxKey = Collections.max(arr.keySet())+1;
-		minKey = Collections.min(arr.keySet());
-		int[] result = {minKey,minValue,maxKey,maxValue};
-        return result;
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+        for(int i=0; i< wallpaper.length;i++ ){
+            for(int j=0; j<wallpaper[i].length();j++){
+                if(wallpaper[i].charAt(j)=='#'){
+                    minX = Math.min(minX,i);
+                    minY = Math.min(minY,j);
+                    maxX = Math.max(maxX,i);
+                    maxY = Math.max(maxY,j);
+                }
+            }
+        }
+        return new int[]{minX,minY,maxX+1,maxY+1};
     }
 }
