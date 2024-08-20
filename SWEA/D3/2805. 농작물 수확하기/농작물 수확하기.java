adf0da45c5f89d099c648c1d21c336de9efec6c9
heesancreat <95956131+heesancreat@users.noreply.github.com>
2024-08-20 17:37:46 +0900

    [D3] Title: 농작물 수확하기, Time: 110 ms, Memory: 18,256 KB -BaekjoonHub

---
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution
+{
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		int N = 0;
+		
+		for(int i=0 ; i<T; i++) {
+			N=Integer.parseInt(br.readLine());
+			int start =N/2;
+			int end =N/2;
+			boolean half = false;
+			int sum=0;
+
+			for(int j=0; j<N; j++) {
+				String str = br.readLine();
+				str = str.substring(start,(end+1));
+				for(int k = 0; k<str.length();k++) {
+					sum+=str.charAt(k)-'0';
+				}
+				if(j==N/2) {
+					half=true;
+				}
+				
+				if(half) {
+					start++;
+					end--;
+				}else {
+					start--;
+					end++;
+				}
+			}
+			sb.append("#"+(i+1)+" " +sum+"\n");
+		}
+		System.out.println(sb);
+	}
+
+}
