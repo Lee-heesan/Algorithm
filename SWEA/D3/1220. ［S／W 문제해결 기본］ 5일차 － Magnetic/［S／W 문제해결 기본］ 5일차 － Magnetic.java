58bef2a3e5eaf74cf6d5fca40e8e899d7503a6c9
heesancreat <95956131+heesancreat@users.noreply.github.com>
2024-08-25 17:58:27 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 246 ms, Memory: 27,956 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
@@ -1,61 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
-class Solution
-{
-	public static void main(String args[])throws IOException {
+public class Solution {
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st ;
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i=0; i<10;i++) {
+		StringTokenizer st;
+		StringBuilder sb= new StringBuilder();
+		int [][] arr = new int [100][100];
+	
+		for(int i=0; i<10; i++) {
 			int length = Integer.parseInt(br.readLine());
-			Map<Integer ,ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
-			int count = 0;
+			int count=0;	
 			
-			//값 넣기
-			for(int j =0;j<100;j++) {
+			for(int j=0; j<length;j++) {
 				st = new StringTokenizer(br.readLine()," ");
-				for(int k=0;k<100;k++) {
-					int num =Integer.parseInt(st.nextToken());
-					if(num==0) {
-						continue;
-					}else {
-						ArrayList<Integer> list = map.get(k);
-						if(list==null) {
-							list=new ArrayList<Integer>();
-							map.put(k, list);
-						}
-						map.get(k).add(num);
-					}
+				for(int k=0; k<length;k++) {
+					arr[j][k]= Integer.parseInt(st.nextToken());
 				}
 			}
 			
-			//교착 상태 출력
-			for(int j=0;j<100;j++) {
-				if(map.get(j).isEmpty()) {
-					continue;
-				}
-				
-				boolean isRed = false;
-				for(int k =0; k<map.get(j).size();k++){
-					if(map.get(j).get(k)==1) {
-						isRed = true;
-					}else if(map.get(j).get(k)==2&&isRed) {
-						count++;
-						isRed=false;
+			for(int j=0; j<length; j++) {
+				int last = 0;
+				for(int k=0; k<length;k++) {
+					 if(arr[k][j]==1) {
+							last=1;
+					}else if(arr[k][j]==2) {
+						if(last==1) {
+							count++;
+						}
+						last=2;
 					}
+					
 				}
 			}
 			
 			sb.append("#").append(i+1).append(" "+count+"\n");
 		}
-		
 		System.out.println(sb.toString());
 	}
+
 }
