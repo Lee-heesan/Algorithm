aa0c723286cefabf7a9531ef29f8499d8327b52a
heesancreat <95956131+heesancreat@users.noreply.github.com>
2024-08-30 17:05:02 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 134 ms, Memory: 24,636 KB -BaekjoonHub

---
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+class Solution
+{
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i<T;i++) {
+			String num = br.readLine();
+			
+			Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+			
+			int a = 0;
+			st = new StringTokenizer(br.readLine()," ");
+			for(int j=0; j<1000; j++) {
+				a = Integer.parseInt(st.nextToken());
+				if(cnt.containsKey(a)) {
+					cnt.put(a, cnt.get(a)+1);
+				}else{
+					cnt.put(a, 1);
+				}
+			}
+			
+			int maxValue = Collections.max(cnt.values());
+			
+			int result=-1;
+			
+			for(int Key : cnt.keySet()) {
+				if(cnt.get(Key)==maxValue) {
+					if(result<Key) {
+						result = Key;
+					}
+				}
+			}
+			
+			sb.append("#").append(num).append(" ").append(result).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+}
