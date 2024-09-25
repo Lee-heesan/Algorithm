319deded1139a3a1df74993e3f87b16212734716
heesancreat <95956131+heesancreat@users.noreply.github.com>
2024-09-25 15:58:42 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 180 ms, Memory: 26,124 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,66 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 class Solution
 {
-	 static int [] arr;
-	public static void main(String args[])throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st;
-    	StringBuilder sb = new StringBuilder();
-    	
-    	int T = Integer.parseInt(br.readLine());
-    	
-    	for(int i = 1 ; i <= T; i++) {
-    		st = new StringTokenizer(br.readLine()," ");
-    		int N = Integer.parseInt(st.nextToken());
-    		int M = Integer.parseInt(st.nextToken());
-    		
-    		arr = new int [N+1];
-    		
-    		for(int j=1; j<N+1;j++) {
-    			arr[j]=j;
-    		}
-    		
-    		
-    		for(int k=0; k<M;k++) {
-    			st=new StringTokenizer(br.readLine()," ");
-    			int from = Integer.parseInt(st.nextToken());
-    			int to = Integer.parseInt(st.nextToken());
-    			unionParent(from,to);
-    		}
-    		
-    		
-    		int count = 0 ;
-    		for(int k =1; k<N+1;k++) {
-    			if(arr[k]==k) {
-    				count++;
-    			}
-    		}
+    static ArrayList<Integer> [] arr;
+	static boolean [] visited;
 
-    		sb.append("#").append(i).append(" ").append(count).append("\n");
-    	}
-    	System.out.println(sb.toString());
-    }
-	public static void unionParent(int from , int to) {
-		int x = getParent(from);
-		int y = getParent(to);
+	static void dfs(int cur) {
+		visited[cur]= true;
 		
-		if(x>y) {
-			arr[x]=y;
-		}else {
-			arr[y]=x;
+		for(int i= 0 ; i < arr[cur].size(); i++) {
+			if(visited[arr[cur].get(i)]) {
+				continue;
+			}
+			dfs(arr[cur].get(i));
 		}
+		
 	}
 	
-	
-	public static int getParent(int A) {
-		if(arr[A]==A) {
-			return A;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i =1; i<=T; i++) {
+			st= new StringTokenizer(br.readLine()," ");
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+
+			arr = new ArrayList [N+1];
+			visited = new boolean[N+1];
+			for(int j=1; j<= N; j++) {
+				arr[j] = new ArrayList<Integer>();
+			}
+			
+			for(int j =0; j<M; j++) {
+				st = new StringTokenizer(br.readLine()," ");
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				arr[from].add(to);
+				arr[to].add(from);
+				
+			}
+			int count =0;
+			for(int j = 1; j <= N; j++) {
+				if(visited[j]) continue;
+				dfs(j);
+				count++;
+			}
+			
+			sb.append("#").append(i).append(" ").append(count).append("\n");
 		}
-		return getParent(arr[A]);
+		
+		System.out.println(sb.toString());
 	}
 }
-
