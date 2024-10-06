5bcd9905b58101dbe20d7bf429effa48c640e261
heesancreat <95956131+heesancreat@users.noreply.github.com>
2024-10-06 17:29:07 +0900

    [Bronze II] Title: 수 정렬하기, Time: 280 ms, Memory: 22256 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,31 +1,41 @@
+
 import java.util.Scanner;
 
 public class Main {
-
+	static int [] arr;
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
-		
+		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
+		arr = new int[N];
 		
-		int [] arr = new int[N];
-		
-		for(int i =0; i<N ; i++) {
-			arr[i]=sc.nextInt();	
+		for(int i = 0; i<N; i++) {
+			arr[i]=sc.nextInt();
 		}
 		
-		for(int i =0; i<N-1 ; i++) {
-			for(int j =i+1; j<N; j++) {
-				if(arr[i]>arr[j]) {
-					int temp=arr[i];
-					arr[i]=arr[j];
-					arr[j]=temp;
-				}
-			}
-		}
+		bubble(N);
 		
-		for(int i =0 ; i<N ; i++) {
+		for(int i=0; i<N; i++) {
 			System.out.println(arr[i]);
 		}
+
+	}
+	
+	public static void bubble(int N) {
+		int count =0; 
+		while(N>count) {
+			for(int i=1; i<arr.length-count; i++) {
+				if(arr[i]<arr[i-1]) {
+					swap(i,(i-1));
+				}
+			}
+			count++;
+		}
+	}
+	
+	public static void swap(int A , int B) {
+		int tmp = arr[B];
+		arr[B] = arr[A];
+		arr[A] = tmp;
 	}
 
 }
