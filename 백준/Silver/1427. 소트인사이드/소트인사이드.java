75b4c03eb5b3ceaecae7635ff5be6291576f0b44
heesancreat <95956131+heesancreat@users.noreply.github.com>
2024-10-14 15:55:09 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,32 +1,49 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
-
+	static int [] arr;
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int num = Integer.parseInt(br.readLine());
-		List<Integer> list = new LinkedList<Integer>();
+		arr = new int[(int)Math.floor(Math.log10(Math.abs(num)))+1];
 		
-		while(num > 0) {
-			list.add(num%10);
+		
+		int i = 0;
+		while(num>0) {
+			arr[i++]=num%10;
 			num=num/10;
 		}
-		int size = list.size();
-		for(int i = 0; i<size;i++) {
-			int max = Integer.MIN_VALUE;
-			for(int j : list) {
-				max = Math.max(max, j);
+		
+
+		for(int j =0; j<arr.length-1; j++) {
+			int max =arr[j];
+			int maxIdx =j;
+			for(int k=j+1; k<arr.length; k++) {
+				if(max<arr[k]) {
+					max = arr[k];
+					maxIdx = k;
+					
+				}
 			}
-			sb.append(max); 
-			list.remove(list.indexOf(max));
+			swap(j,maxIdx);
 		}
+		
+		for(int j =0; j<arr.length; j++) {
+			sb.append(arr[j]);
+		}
+		
 		System.out.println(sb.toString());
+
+	}
+	
+	
+	public static void swap(int A, int B) {
+		int temp = arr[A];
+		arr[A]= arr[B];
+		arr[B]= temp;
 	}
 
 }
