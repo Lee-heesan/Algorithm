962da4ddb3b0a27fb14366661f6fed5993ad2573
heesancreat <95956131+heesancreat@users.noreply.github.com>
2024-10-15 15:35:58 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 151 ms, Memory: 20,716 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -3,27 +3,26 @@ import java.util.Scanner;
 
 class Solution
 {
-	public static void main(String args[])throws Exception{
+	public static void main(String[] args) throws Exception{
 		Scanner sc = new Scanner(System.in);
 		
 		for(int i =1; i<=10; i++) {
 			int seq = sc.nextInt();
 			long N = sc.nextLong();
 			int M = sc.nextInt(); 			
-			long result= pow(N,N,M-1);
+			long result= pow(N,M-1);
 
 
 			System.out.println("#"+seq+" "+result);
 		}
-
 	}
 	
 	
-	public static long pow(long now ,long N, int M) {
+	public static long pow(long N, int M) {
 		if(M==0) {
-			return now;
+			return N;
 		}
-		return pow(now*N,N,M-1);
+		return N*pow(N,M-1);
 	}
 
 }
