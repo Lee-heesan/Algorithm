8180086210596e9339e299cee148ad885c50fd94
heesancreat <95956131+heesancreat@users.noreply.github.com>
2024-10-25 15:50:04 +0900

    [Silver II] Title: 예산, Time: 152 ms, Memory: 15304 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.java
@@ -0,0 +1,57 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int [] arr;
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		int lo =  1;
+		int hi = 0;
+		int sum = 0;
+		for(int i = 0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			sum += arr[i]; 
+			hi = Math.max(hi, arr[i]);
+		}
+		
+		int total = Integer.parseInt(br.readLine());
+		
+		if(sum<=total) {
+			System.out.println(hi);
+			return;
+		}
+		
+		int mid = (lo+hi)/2;
+		sum = 0;
+		
+		while(lo<hi-1) {
+			mid = (lo+hi)/2;
+			sum = 0;
+			for(int i =0; i<N; i++) {
+				sum += Math.min(arr[i],mid);
+			}
+			
+			if(sum<=total) {
+				lo = mid;
+			}else {
+				hi = mid;
+			}
+			
+		}
+
+		System.out.println(lo);
+		
+		
+		
+	}
+
+}
