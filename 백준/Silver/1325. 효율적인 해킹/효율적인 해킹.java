a620f3614d573736255fa6cc39e5c88348c6f173
heesancreat <95956131+heesancreat@users.noreply.github.com>
2024-11-15 17:29:17 +0900

    [Silver I] Title: 효율적인 해킹, Time: 9464 ms, Memory: 305560 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -3,68 +3,76 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static List<Integer> [] list;
+	static Queue<Integer> queue = new LinkedList<>();
+	static ArrayList<Integer> [] list; 
+	static int [] D;
 	static boolean [] visited;
-	static int [] count; 
-	public static void main(String[] args)throws IOException {
+	public static void main(String [] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		
+		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		
 		list = new ArrayList[N+1];
-		visited = new boolean[N+1];
-		count = new int[N+1];
-		
-		for(int i = 1; i<=N; i++) {
-			list[i] = new ArrayList<Integer>();
+		for(int i =1 ; i<=N; i++) {
+			list[i] = new ArrayList<>();
 		}
 		
 		for(int i =0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			
 			int A = Integer.parseInt(st.nextToken());
 			int B = Integer.parseInt(st.nextToken());
 			
 			list[A].add(B);
 		}
 		
+		D = new int[N+1];
 		for(int i=1; i<=N; i++) {
-			visited = new boolean[N+1];
+			visited= new boolean[N+1];
 			
-			visited[i]=true;
-			count[i]++;
-			DFS(i);
+			visited[i] =true;
+			D[i]++;
+			queue.add(i);
+			
+			bfs();
 		}
 		
-		int maxCount =0;
-		for(int i=1; i<=N; i++) {
-			maxCount = Math.max(maxCount, count[i]);
+		
+		int maxCount = 0;
+		for (int i = 1; i <= N; i++) {
+			maxCount = Math.max(maxCount, D[i]);
 		}
 		
-		for(int i =1; i<=N; i++) {
-			if(maxCount==count[i]) {
-				sb.append(i+" ");
+		for (int i = 1; i <= N; i++) {
+			if (D[i] == maxCount) {
+				sb.append(i).append(" ");
 			}
 		}
 		
 		System.out.println(sb);
+		
 	}
 	
-	public static void DFS(int num) {
-		for(int i: list[num]) {
-			if(!visited[i]) {
-				visited[i]=true;
-				count[i]++;
-				DFS(i);
+	public static void bfs() {
+		while(!queue.isEmpty()) {
+			int now = queue.remove();
+			
+			for(int i : list[now]) {
+				if(!visited[i]) {
+					D[i]++;
+					visited[i]=true;
+					queue.add(i);
+				}
 			}
 		}
 	}
-
 }
