7e617b99898da1e642adf7074dace7884939cc51
heesancreat <95956131+heesancreat@users.noreply.github.com>
2024-11-16 00:34:36 +0900

    [Gold IV] Title: 여행 가자, Time: 160 ms, Memory: 17324 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,26 +1,19 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	// 1976 유니온 파인드
 	static int [] arr;
-	public static void main(String[] args) throws IOException {
+	public static void main(String [] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
+		StringTokenizer st ;
+		StringBuilder sb = new StringBuilder();
+	
 		int N = Integer.parseInt(br.readLine());
 		int M = Integer.parseInt(br.readLine());
-		
-		
 		arr = new int[N+1];
-		for(int i=1; i<=N;i++) {
-			arr[i]= i;
+		for(int i=1; i<=N; i++) {
+			arr[i]=i;
 		}
 		
 		for(int i=1; i<=N; i++) {
@@ -28,27 +21,22 @@ public class Main {
 			for(int j=1; j<=N; j++) {
 				int num = Integer.parseInt(st.nextToken());
 				if(num==1) {
-					if(find(i)==find(j)) {
-						continue;
-					}else {
+					if(find(i)!=find(j)) {
 						union(i,j);
 					}
 				}
 			}
 		}
 		
-		int [] plan = new int[M];
 		st = new StringTokenizer(br.readLine());
-		for(int i =0; i<M;i++) {
-			plan[i]=Integer.parseInt(st.nextToken());	
+		int [] plan = new int[M];
+		for(int i=0; i<M; i++) {
+			plan[i]=Integer.parseInt(st.nextToken());
 		}
 		
 		
-		for(int i=0; i<M-1;i++) {
-			int num1 = plan[i];
-			int num2 = plan[i+1];
-			
-			if(find(num1)!=find(num2)) {
+		for(int i =0; i<M-1; i++) {
+			if(find(plan[i])!=find(plan[i+1])) {
 				System.out.println("NO");
 				return;
 			}
@@ -58,21 +46,23 @@ public class Main {
 		
 	}
 	
-	public static void union(int num1, int num2) {
-		int rootX = find(num1);
-		int rootY = find(num2);
+	public static void union(int A, int B) {
+		int rootA = find(A);
+		int rootB = find(B);
 		
-		if(rootX!=rootY) {
-			arr[rootX]=rootY;
+		
+		if(rootA == rootB) {
+			return;
+		}else {
+			arr[rootA]=rootB;
 		}
 	}
-	
-	
 	
 	public static int find(int num) {
 		if(arr[num]==num) {
 			return num;
 		}
-		return arr[num]= find(arr[num]);
+		
+		return arr[num] = find(arr[num]);
 	}
 }
