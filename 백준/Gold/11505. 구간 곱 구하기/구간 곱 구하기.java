242d58183d75807504caed1a9f0d04a64ad4bc04
Hee san <heesan2572@gmail.com>
2025-01-29 01:35:24 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 452 ms, Memory: 87272 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
@@ -5,86 +5,86 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static long[] tree;
-    static int size;
-    static int n, m, k;
+	static long [] tree;
+	static int size;
+	static int n, m, k;
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		
+		size = 1;
+		while(size<n) {
+			size *= 2;
+		}
+		tree = new long[size*2];
+		
+		for(int i=size; i<size+n; i++) {
+			tree[i] =Long.parseLong(br.readLine());
+		}
+		
+		build();
+		
+		for(int i = 0; i<m+k; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			long c = Integer.parseInt(st.nextToken());
+			
+			if(a==1) {
+				update(size+b-1,c);
+			}else if (a==2) {
+				sb.append(query(size+b-1,size+(int)c-1)).append("\n");
+			}
+		}
+		
+		System.out.println(sb.toString());
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+	}
+	
+	private static long query(int start, int end) {
+	    long paramSum = 1;
 
-        // n, m, k 입력
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+	    while (start <= end) {
+	        if (start == end) {
+	            paramSum = (paramSum * tree[start]) % 1000000007; 
+	            break;
+	        }
 
-        size = 1;
-        while (size < n) {
-            size *= 2;
-        }
-        tree = new long[size * 2];
+	        if (start % 2 == 1) { 
+	            paramSum = (paramSum * tree[start]) % 1000000007; 
+	            start++;
+	        }
+	        if (end % 2 == 0) { 
+	            paramSum = (paramSum * tree[end]) % 1000000007; 
+	            end--;
+	        }
 
-        // 트리 초기화
-        for (int i = size; i < size + n; i++) {
-            tree[i] = Long.parseLong(br.readLine());
-        }
+	        start /= 2;
+	        end /= 2;
+	    }
 
-        build(); // 트리 구성
+	    return paramSum;
+	}
+	
+	private static void update(int idx, long value) {
+	    tree[idx] = value;
 
-        // 쿼리 처리
-        for (int i = 0; i < m + k; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            long c = Long.parseLong(st.nextToken());
+	    while (idx > 1) {
+	        idx /= 2;
+	        tree[idx] = (tree[idx * 2] * tree[idx * 2 + 1]) % 1000000007; 
+	    }
+	}
 
-            if (a == 1) {
-                update(size + b - 1, c); // b번째 값을 c로 갱신
-            } else if (a == 2) {
-                sb.append(query(size + b - 1, size + (int) c - 1)).append("\n"); // b부터 c까지 곱셈 쿼리
-            }
-        }
+	private static void build() {
+	    for (int i = size - 1; i > 0; i--) {
+	        tree[i] = (tree[i * 2] * tree[i * 2 + 1]) % 1000000007; 
+	    }
+	}
 
-        System.out.println(sb.toString());
-    }
-
-    // 구간 곱셈 쿼리
-    private static long query(int start, int end) {
-        long paramSum = 1;
-
-        while (start <= end) {
-            if (start % 2 == 1) { // start가 오른쪽 자식이면 해당 값 포함
-                paramSum = (paramSum * tree[start]) % 1000000007; // 곱셈 후 모듈러 연산
-                start++;
-            }
-            if (end % 2 == 0) { // end가 왼쪽 자식이면 해당 값 포함
-                paramSum = (paramSum * tree[end]) % 1000000007;
-                end--;
-            }
-
-            start /= 2;
-            end /= 2;
-        }
-
-        return paramSum;
-    }
-
-    // 업데이트 함수
-    private static void update(int idx, long value) {
-        tree[idx] = value;
-
-        // 업데이트된 값을 부모까지 전파
-        while (idx > 1) {
-            idx /= 2;
-            tree[idx] = (tree[idx * 2] * tree[idx * 2 + 1]) % 1000000007; // 곱셈 후 모듈러 연산
-        }
-    }
-
-    // 트리 초기화
-    private static void build() {
-        for (int i = size - 1; i > 0; i--) {
-            tree[i] = (tree[i * 2] * tree[i * 2 + 1]) % 1000000007; // 곱셈 후 모듈러 연산
-        }
-    }
 }
