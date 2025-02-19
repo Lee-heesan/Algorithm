79974d807d845627086fc91eeeaa90f82beb988b
Hee san <heesan2572@gmail.com>
2025-02-19 11:49:17 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 584 ms, Memory: 142148 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,61 +1,57 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-	static Stack<Integer>[] arr;
-	static boolean visited[];
+	static List<Integer> [] list;
+	static boolean [] visited;
 	static int count;
-	public static void main(String[] args) throws IOException{
+	public static void main(String [] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		visited = new boolean[N+1];
-		arr = new Stack[N+1];
-		
-		for(int i=1 ; i<=N; i++) {
-			arr[i] = new Stack<Integer>();
-		}
-		
-		for(int i =0 ; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());			
-			int end = Integer.parseInt(st.nextToken());
-			arr[start].add(end);
-			arr[end].add(start);
-		}
-		
-		count = 0;
+		list = new ArrayList[N+1];
 		for(int i=1; i<=N; i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			
+			list[u].add(v);
+			list[v].add(u);
+		}
+		
+		count =0;
+		for(int i =1; i<=N; i++) {
 			if(visited[i]) {
 				continue;
 			}
-			visited[i]=true;
 			DFS(i);
 			count++;
 		}
 		
 		System.out.println(count);
-		
 	}
 	
-	public static void DFS(int i) {		
-		while(!arr[i].isEmpty()) {
-			visited[arr[i].peek()]=true;
-			DFS(arr[i].pop());
-		}
-		
-		if(arr[i].isEmpty()) {
+	private static void DFS(int v) {
+		if(visited[v]) {
 			return;
 		}
 		
+		visited[v] = true;
+		for(int i : list[v]) {
+			if(!visited[i]) {
+				DFS(i);
+			}
+		}
 	}
-
 }
