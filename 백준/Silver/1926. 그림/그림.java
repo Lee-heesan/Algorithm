b99c605e47e72ca8a905b46b406987419102d37b
Hee san <heesan2572@gmail.com>
2026-03-04 13:05:32 +0900

    [Silver I] Title: 그림, Time: 340 ms, Memory: 52968 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -30,45 +28,32 @@ public class Main {
 		}
 		
 		int count = 0;
-
-		for(int i=0;i<n;i++){
-		    for(int j=0;j<m;j++){
-		        if(arr[i][j]==1 && !visited[i][j]){
-		            int area = bfs(i,j);
+		for(int i =0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				if(arr[i][j]==1 && !visited[i][j]) {
+					int area = dfs(i, j);
 		            count++;
 		            max = Math.max(max, area);
-		        }
-		    }
+				}
+			}
 		}
 		
 		System.out.println(count);
 		System.out.println(max);
 	}
 	
-	static int bfs(int y, int x){
-	    Queue<int[]> q = new LinkedList<>();
-	    q.add(new int[]{y,x});
+	private static int dfs(int y, int x) {
 	    visited[y][x] = true;
-
 	    int area = 1;
 
-	    while(!q.isEmpty()){
-	        int[] cur = q.poll();
-	        int cy = cur[0];
-	        int cx = cur[1];
+	    for (int i = 0; i < 4; i++) {
+	        int ny = y + dy[i];
+	        int nx = x + dx[i];
 
-	        for(int i=0;i<4;i++){
-	            int ny = cy + dy[i];
-	            int nx = cx + dx[i];
-
-	            if(ny>=0 && ny<n && nx>=0 && nx<m){
-	                if(arr[ny][nx]==1 && !visited[ny][nx]){
-	                    visited[ny][nx] = true;
-	                    q.add(new int[]{ny,nx});
-	                    area++;
-	                }
-	            }
+	        if (ny >= 0 && ny < n && nx >= 0 && nx < m && !visited[ny][nx] && arr[ny][nx] == 1) {
+	            area += dfs(ny, nx);
 	        }
 	    }
 	    return area;
-	}}
+	}
+}
