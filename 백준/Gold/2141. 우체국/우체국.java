4bd471ce67d0da921f929ef7058e39e12da4caf6
Hee san <heesan2572@gmail.com>
2026-03-05 14:48:37 +0900

    [Gold IV] Title: 우체국, Time: 520 ms, Memory: 48340 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.java
+++ 백준/Gold/2141. 우체국/우체국.java
@@ -1,38 +1,47 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.*;
 
 public class Main {
-    static class Node {
-        long x, a;
-        Node(long x, long a) { this.x = x; this.a = a; }
-    }
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		
+		node [] arr = new node [N];
+		long total = 0;
+		
+		for(int i = 0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int village = Integer.parseInt(st.nextToken());
+			int human = Integer.parseInt(st.nextToken());
+			arr[i] = new node(village,human);
+			total += human;
+		}
+				
+		 Arrays.sort(arr, Comparator.comparingLong(o -> o.num));
 
-        Node[] arr = new Node[N];
-        long total = 0;
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            long x = Long.parseLong(st.nextToken());
-            long a = Long.parseLong(st.nextToken());
-            arr[i] = new Node(x, a);
-            total += a;
-        }
-
-        Arrays.sort(arr, Comparator.comparingLong(o -> o.x));
-
-        long target = (total + 1) / 2; // 절반 이상(올림)
-        long prefix = 0;
-
-        for (Node n : arr) {
-            prefix += n.a;
-            if (prefix >= target) {
-                System.out.println(n.x);
-                return;
-            }
-        }
-    }
+	     long target = (total + 1) / 2;
+	     long prefix = 0;
+	     
+	     for (node n : arr) {
+	            prefix += n.people;
+	            if (prefix >= target) {
+	                System.out.println(n.num);
+	                return;
+	            }
+	        }
+	}
+	
+	static class node{
+		long num;
+		long people;
+		
+		public node(long num, long people) {
+			this.num = num;
+			this.people = people;
+		}
+	}
 }
