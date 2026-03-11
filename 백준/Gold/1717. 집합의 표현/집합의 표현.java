75687602feaf94371720d63458d2e1b831015003
Hee san <heesan2572@gmail.com>
2026-03-11 15:45:38 +0900

    [Gold V] Title: 집합의 표현, Time: 784 ms, Memory: 68724 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,63 +1,63 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    //1717
 	static int [] arr;
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		
-		arr = new int[N+1];
-		for(int i =1; i<=N; i++) {
+		arr = new int [n+1];
+		
+		for(int i = 1; i<=n; i++) {
 			arr[i] = i;
 		}
 		
-		for(int i= 0; i<M; i++) {
+		
+		int a = -1;
+		int b = -1;
+		int c = -1;
+		for(int i =0; i<m; i++) {
 			st = new StringTokenizer(br.readLine());
 			
-			int H = Integer.parseInt(st.nextToken());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			if(H==1) {
-				if(find(A)==find(B)) {
+			c = Integer.parseInt(st.nextToken());
+			
+			if(c==1) {
+				a = Integer.parseInt(st.nextToken());
+				b = Integer.parseInt(st.nextToken());
+				if(find(a)==find(b)) {
 					sb.append("YES"+"\n");
 				}else {
 					sb.append("NO"+"\n");
 				}
-			}else {
-				union(A,B);
+				
+			}else if (c==0) {
+				a = Integer.parseInt(st.nextToken());
+				b = Integer.parseInt(st.nextToken());
+				union(a,b);
 			}
 		}
 		
 		System.out.println(sb.toString());
-
 	}
 	
-	public static void union(int num1 ,int num2) {
-		int rootX = find(num1);
-        int rootY = find(num2);
-
-        if (rootX != rootY) { // 루트가 다를 때만 합침
-            arr[rootY] = rootX; // y의 루트를 x의 루트로 변경
-        }
+	
+	public static void union(int a, int b) {
+		int A = find(a);
+		int B = find(b);
 		
+		arr[B] = A;
 	}
-	
 	
 	public static int find(int num) {
-		if(num==arr[num]) {
+		if(arr[num]==num) {
 			return num;
 		}
 		
 		return arr[num] = find(arr[num]);
 	}
-
 }
