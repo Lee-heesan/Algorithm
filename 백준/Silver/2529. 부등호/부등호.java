4ae6816f52748bd4cf6c818dab1d49d983fbc989
Hee san <heesan2572@gmail.com>
2026-03-18 16:10:37 +0900

    [Silver I] Title: 부등호, Time: 200 ms, Memory: 22084 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -1,50 +1,47 @@
 import java.io.*;
 
-public class Main{
+public class Main {
 	 static int k;
-	    static String[] sign;
-	    static boolean[] visited = new boolean[10];
-	    static String min = null;
-	    static String max = null;
-
-	    public static void main(String[] args) throws IOException {
-	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-	        k = Integer.parseInt(br.readLine());
-	        sign = br.readLine().split(" ");
-
-	        dfs(0, "");
-
-	        System.out.println(max);
-	        System.out.println(min);
-	    }
-
-	    static void dfs(int depth, String num) {
-	        if (depth == k + 1) {
-	            if (min == null) {
-	            	min = num;
-	            }
-	            max = num;
-	            return;
-	        }
-
-	        for (int i = 0; i <= 9; i++) {
-	            if (visited[i]) {
-	            	continue;
-	            }
-
-	            if (depth == 0 || check(num.charAt(depth - 1) - '0', i, sign[depth - 1])) {
-	                visited[i] = true;
-	                dfs(depth + 1, num + i);
-	                visited[i] = false;
-	            }
-	        }
-	    }
-
-	    static boolean check(int a, int b, String op) {
-	        if (op.equals("<")) {
-	        	return a < b;
-	        }
-	        return a > b;
-	    }
+	 static String [] sign;
+	 static boolean [] visited;
+	 static String max=null;
+	 static String min=null;
+	 public static void main(String[] args) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 k = Integer.parseInt(br.readLine());
+		 sign = br.readLine().split(" ");
+		 visited = new boolean[10];
+		 dfs(0,"");
+		 
+		 System.out.println(max);
+		 System.out.println(min);
+	 }
+	 
+	 public static void dfs(int depth, String str) {
+		 if(depth>k) {
+			 if(min==null) {
+				 min = str;
+			 }
+			 max=str;
+			 return;
+		 }
+		 
+		 for(int i =0; i<=9; i++) {
+			 if(visited[i]) {
+				continue; 
+			 }
+			 if(depth==0 || check(str.charAt(depth - 1) - '0',i,sign[depth - 1])) {
+				 visited[i] = true;
+	             dfs(depth + 1, str + i);
+	             visited[i] = false;
+			 }
+		}
+	 }
+	 
+	 public static boolean check(int a, int b, String str) {
+		 if(str.equals(">")) {
+			 return a>b;
+		 }
+		 return a<b;
+	 }
 }
