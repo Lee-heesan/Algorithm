da670d9cbafeeee841665ad8a8395c08f1e830f0
Hee san <heesan2572@gmail.com>
2026-04-15 13:40:19 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 168 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,37 +1,26 @@
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-	static int count;
-	public static void main(String[] args) throws IOException{
-		Scanner sc =new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		
-		int T = sc.nextInt();
-		
-		for(int i =0; i<T; i++) {
-			int num = sc.nextInt();
-			count = 0;
-			cal(num);
-			sb.append(count).append("\n");
-		}
-		
-		System.out.println(sb.toString());
-	}
-	
-	public static void cal(int num) {
-		if(num==0) {
-			count++;
-			return;
-		}
-		
-		
-		for(int i = 1; i<=3; i++) {
-			int now = num -i;
-			
-			if(now>=0) {
-				cal(now);
-			}
-		}
-	}
+public class Main{
+    static int [] dp = new int [11];
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int T = sc.nextInt();
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        
+        for(int i =4; i<11; i++){
+            dp[i] = dp[i-1] + dp[i-2]+ dp[i-3];
+        }
+        
+        for(int i=0; i<T; i++){
+            int num = sc.nextInt();
+            sb.append(dp[num]).append("\n");
+        }
+        
+        System.out.println(sb.toString());
+    }
 }
