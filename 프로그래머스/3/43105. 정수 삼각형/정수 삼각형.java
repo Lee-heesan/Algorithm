e0539db242ef6bb92c5067f74e667b1ab4f57a0c
Hee san <heesan2572@gmail.com>
2026-04-30 23:59:02 +0900

    [level 3] Title: 정수 삼각형, Time: 9.68 ms, Memory: 63.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] triangle) {
+        int n = triangle.length;
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                
+                if(j == 0){
+                    triangle[i][j] += triangle[i-1][j];
+                } else if(j == i){
+                    triangle[i][j] += triangle[i-1][j-1];
+                } else {
+                    triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
+                }
+            }
+        }
+        
+        int answer = 0;
+        for(int x : triangle[n-1]){
+            answer = Math.max(answer, x);
+        }
+        
+        return answer;
+    }
+}
