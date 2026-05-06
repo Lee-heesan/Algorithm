e0f8b50ed002ef21cdc25c0b2be2b75dd2434852
Hee san <heesan2572@gmail.com>
2026-05-06 16:13:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 92.92 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String c : completion){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        
+        for(String p :participant){
+            if(!map.containsKey(p)){
+                answer = p;
+            }else{
+                if(map.get(p)<1){
+                    answer = p;
+                }
+                map.put(p, map.get(p)-1);
+            }
+        }
+        return answer;
+    }
+}
